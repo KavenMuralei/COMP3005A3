544a7d8b01c724c8e2d9bac95664f95bc1fa7e90
Kaven Muraleitharan <kavenmuraleitharan0726@gmail.com>
2025-11-09 16:55:24 -0500

    Added some documentation aswell as a readme file. Still need to add more information in the readme.

---
+++ README.md
@@ -0,0 +1,35 @@
+## Kaven Muraleitharan
+#### SN: 101305963
+#### COMP3005 A3
+
+### Description
+This is an application which performs basic CRUD operations on a table of students
+
+### Implemented functions
+getAllStudents(): Retrieves and displays all student records from the database
+
+addStudent(first_name, last_name, email, enrollment_date): Inserts a new student into the database
+
+updateStudentEmail(student_id, new_email): Updates the email of a student with the specified ID
+
+deleteStudent(student_id): Deletes a student record based on their ID
+
+### Database setup
+```
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    enrollment_date DATE
+);
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
+```
+
+### How to run
+
+1. Using pgAdmin, create a new database and execute the database setup listed above
+2. Run the project through intellij using Maven
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+//Kaven Muraleitharan
+//SN: 101305963
+
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -5,56 +8,21 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        int options = -1;
-        Scanner scanner = new Scanner(System.in);
-        do {
-            System.out.println("Select an option to edit the students table");
-            System.out.println("0 - Exit");
-            System.out.println("1 - Get all students");
-            System.out.println("2 - Add a student");
-            System.out.println("3 - Update a student email");
-            System.out.println("4 - Delete a student");
-            options = Integer.parseInt(scanner.nextLine());
+//    String url = "jdbc:postgresql://localhost:5432/a3";
+//    String user = "postgres";
+//    String password = "admin";
+    private static String url; //link to the database with the student table
+    private static String user; // username of the database
+    private static String password;//password for the database
 
-            if(options == 1){
-                getAllStudents();
-            }
-            else if(options == 2){
-                try{
-                    System.out.println("Enter a first name");
-                    String firstName= scanner.nextLine();
-                    System.out.println("Enter a last name");
-                    String lastName = scanner.nextLine();
-                    System.out.println("Enter an email");
-                    String email = scanner.nextLine();
-                    System.out.println("Enter an enrollment date (YYYY-MM-DD)");
-                    String date = scanner.nextLine();
-                    addStudent(firstName,lastName,email,date);
-                }catch (Exception e){
-                   System.out.println("Error occurred when adding student: "+e);
-                }
-            }
-            else if (options ==3 ) {
-                System.out.println("Enter the student ID you want to delete");
-                int studentID = Integer.parseInt(scanner.nextLine());
-                System.out.println("Enter the new email");
-                String email = scanner.nextLine();
-                updateStudentEmail(studentID,email);
-            }
-            else if (options ==4){
-                System.out.println("Enter the student ID you want to delete");
-                int studentID = Integer.parseInt(scanner.nextLine());
-                deleteStudent(studentID);
-            }
 
-        } while (options != 0);
-    }
     public static void getAllStudents(){
+        /*
+        displays all student records in the students table
+
+        throws: exception if there is a database connection or query error
+         */
         try{
-            String url = "jdbc:postgresql://localhost:5432/a3";
-            String user = "postgres";
-            String password = "admin";
             Class.forName("org.postgresql.Driver");
             Connection connection = DriverManager.getConnection(url,user,password);
             Statement statement = connection.createStatement();
@@ -71,14 +39,22 @@ public class Main {
             statement.close();
             connection.close();
         }catch(Exception e){
-            System.out.println(e);
+            System.out.println("Error when collecting the students: "+e);
         }
     }
     public static void addStudent(String first_name, String last_name, String email, String enrollment_date){
+        /*
+        adds a new student record into the students table
+
+        param: String first_name - the students first name
+        param: String last_name - the students last name
+        param: String email - the students email
+        param: String enrollment_date - the students enrollment date (yy-mm-dd)
+
+        throws: exception if the database connection fails or the SQL statement has an error.
+        (typically if the SQL statement has an error, then the user inputted the incorrect format for the database)
+         */
         try{
-            String url = "jdbc:postgresql://localhost:5432/a3";
-            String user = "postgres";
-            String password = "admin";
             Class.forName("org.postgresql.Driver");
             Connection connection = DriverManager.getConnection(url,user,password);
             Statement statement = connection.createStatement();
@@ -86,15 +62,21 @@ public class Main {
             statement.close();
             connection.close();
         }catch(Exception e){
-            System.out.println(e);
+            System.out.println("Error when adding the student: "+ e);
         }
     }
 
     public static void updateStudentEmail(int student_id, String email){
+        /*
+        updates the email of an existing student record given their student id
+
+        param: int student_id - the student id of the student we are trying to update
+        param: String email - the new email
+
+        throws: exception if the database connection fails or the SQL statement has an error.
+        (typically if the SQL statement has an error, then the user inputted the incorrect format for the database)
+         */
         try{
-            String url = "jdbc:postgresql://localhost:5432/a3";
-            String user = "postgres";
-            String password = "admin";
             Class.forName("org.postgresql.Driver");
             Connection connection = DriverManager.getConnection(url,user,password);
             Statement statement = connection.createStatement();
@@ -102,11 +84,19 @@ public class Main {
             statement.close();
             connection.close();
         }catch(Exception e){
-            System.out.println(e);
+            System.out.println("Error when updating the students email: "+e);
         }
     }
 
     public static void deleteStudent(int student_id){
+        /*
+        delete a student record from the table give their student id
+
+        param: int student_id - the student id of te student we are trying to delete
+
+        throws: exception if the database connection fails or the SQL statement has an error.
+        (typically if the SQL statement has an error, then the user inputted the incorrect format for the database)
+         */
         try{
             String url = "jdbc:postgresql://localhost:5432/a3";
             String user = "postgres";
@@ -118,7 +108,87 @@ public class Main {
             statement.close();
             connection.close();
         }catch(Exception e){
-            System.out.println(e);
+            System.out.println("Error when deleting a student "+e);
         }
     }
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        while (true) { //Loop to ensure the user inputs url, user, and pass.
+
+            try {
+                System.out.println("Enter database url:");
+                url = scanner.nextLine();
+                System.out.println("Enter database username:");
+                user = scanner.nextLine();
+                System.out.println("Enter database password:");
+                password = scanner.nextLine();
+                Class.forName("org.postgresql.Driver");
+                Connection connection = DriverManager.getConnection(url, user, password);
+                if (connection != null) {
+                    System.out.println("Connected to database");
+                    break;
+                } else {
+                    System.out.println("Failed to connect to database.");
+                }
+            } catch (Exception e) {
+                System.out.println("Connection Failed: " + e);
+            }
+        }
+        int options =-1;
+        // Loop which executes the implemented functions
+        do {
+            try{
+                System.out.println("Select an option to edit the students table");
+                System.out.println("0 - Exit");
+                System.out.println("1 - Get all students");
+                System.out.println("2 - Add a student");
+                System.out.println("3 - Update a student email");
+                System.out.println("4 - Delete a student");
+                options = Integer.parseInt(scanner.nextLine());
+            }catch (Exception e){
+                System.out.println("Error in inputted text: "+e);
+            }
+            if(options == 1){
+                getAllStudents();
+            }
+            else if(options == 2){
+                try{ //try catch to prevent input errors
+                    System.out.println("Enter a first name");
+                    String firstName= scanner.nextLine();
+                    System.out.println("Enter a last name");
+                    String lastName = scanner.nextLine();
+                    System.out.println("Enter an email");
+                    String email = scanner.nextLine();
+                    System.out.println("Enter an enrollment date (YYYY-MM-DD)");
+                    String date = scanner.nextLine();
+                    addStudent(firstName,lastName,email,date);
+                }catch (Exception e){
+                   System.out.println("Error occurred in the syntax of entered text: "+e);
+                }
+            }
+            else if (options ==3 ) {
+                try{//try catch to prevent input errors
+                    System.out.println("Enter the student ID you want to delete");
+                    int studentID = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Enter the new email");
+                    String email = scanner.nextLine();
+                    updateStudentEmail(studentID,email);
+                }catch (Exception e){
+                    System.out.println("Error occurred in the syntax of entered text: "+e);
+                }
+
+            }
+            else if (options ==4){
+                try{//try catch to prevent input errors
+                    System.out.println("Enter the student ID you want to delete");
+                    int studentID = Integer.parseInt(scanner.nextLine());
+                    deleteStudent(studentID);
+                }catch(Exception e){
+                    System.out.println("Error occurred in the syntax of entered text: "+e);
+                }
+            }
+        } while (options != 0);
+    }
+
 }
